264118007a374e4371b20489e8600ea7e013e2bb
jbbb92723-cyber <jbbb92723@gmail.com>
2026-04-24 18:43:15 +0800

    chore: source change to trigger production build (env vars update)

--- zeno-site/app/layout.tsx
+++ zeno-site/app/layout.tsx
@@ -5,6 +5,8 @@ import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 import '@/styles/globals.css'
 
+// Build trigger: 2026-04-24 env vars update
+
 const inter = Inter({
   subsets: ['latin'],
   variable: '--font-inter',
